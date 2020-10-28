86d090c856b43e0a7b0be1cd002528ea2f38f262
igres26 <igres26@gmail.com>
2020-10-28 15:53:55 +0400

    added core of program

---
+++ functions.py
@@ -0,0 +1,124 @@
+import sympy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_file(file_name, instance):
+    """Collect data from .txt file that characterizes the problem instance.
+    Args:
+        file_name (str): name of the file that contains the instance information.
+        instance (str): number of intance to use.
+
+    Returns:
+        control (list): important parameters of the instance.
+            [number of qubits, number of clauses, number of ones in the solution]
+        solution (list): list of the correct outputs of the instance for testing.
+        clauses (list): list of all clauses, with the qubits each clause acts upon.
+    """
+    file = open('../data3sat/{q}bit/n{q}i{i}.txt'.format(q=file_name, i=instance), 'r')
+    control = list(map(int, file.readline().split()))
+    solution = list(map(str, file.readline().split()))
+    clauses = [list(map(int, file.readline().split())) for _ in range(control[1])]
+    return control, solution, clauses
+
+
+def times(qubits, clauses):
+    """Count the times each qubit appears in a clause to normalize H0.
+    Args:
+        qubits (int): # of total qubits in the instance.
+        clauses (list): clauses of the Exact Cover instance.
+
+    Returns:
+        times (list): number of times a qubit apears in all clauses.
+    """
+    times = np.zeros(qubits)
+    for clause in clauses:
+        for num in clause:
+            times[num-1] += 1
+    return times
+
+
+def h_problem(qubits, clauses):
+    """Hamiltonian that satisfies all Exact Cover clauses.
+    Args:
+        qubits (int): # of total qubits in the instance.
+        clauses (list): clauses for an Exact Cover instance.
+
+    Returns:
+        sham (sympy.Expr): Symbolic form of the problem Hamiltonian.
+        smap (dict): Dictionary that maps the symbols that appear in the
+            Hamiltonian to the corresponding matrices and target qubits.
+    """
+    z = sympy.symbols(" ".join((f"z{i}" for i in range(qubits))))
+    z_matrix = (matrices.I - matrices.Z) / 2.0
+    smap = {s: i for i, s in enumerate(z)}
+    sham = sum((sum(z[i - 1] for i in clause) - 1) ** 2 for clause in clauses)
+    return sham, smap
+
+
+def h_weights(qubits, clauses, times):
+    """Hamiltonian that satisfies all Exact Cover clauses.
+    Args:
+        qubits (int): # of total qubits in the instance.
+        clauses (list): clauses for an Exact Cover instance.
+
+    Returns:
+        sham (sympy.Expr): Symbolic form of the problem Hamiltonian.
+        smap (dict): Dictionary that maps the symbols that appear in the
+            Hamiltonian to the corresponding matrices and target qubits.
+    """
+    z = sympy.symbols(" ".join((f"z{i}" for i in range(qubits))))
+    z_matrix = (matrices.I - matrices.Z) / 2.0
+    smap = {s: i for i, s in enumerate(z)}
+    sham = sum((sum(z[i - 1] for i in clause) - 1) ** 2 for clause in clauses)
+    return sham, smap
+
+
+def symbolic_to_dwave(symbolic_hamiltonian, symbol_num):
+    """Transforms a symbolic Hamiltonian to a dictionary of targets and matrices.
+    
+    Works for Hamiltonians with one and two qubit terms only.
+    
+    Args:
+        symbolic_hamiltonian: The full Hamiltonian written with symbols.
+        symbol_num: Dictionary that maps each symbol that appears in the 
+            Hamiltonian to its target.
+    
+    Returns:
+       Q (dict): Dictionary with the interactions to send to the DWAVE machine.
+       overall_constant (int): Constant that cannot be given to DWAVE machine.
+    """ 
+    Q = {}
+    overall_constant = 0
+    for term in symbolic_hamiltonian.args:
+        if not term.args:
+            expression = (term,)
+        else:
+            expression = term.args
+
+        symbols = [x for x in expression if x.is_symbol]
+        numbers = [x for x in expression if not x.is_symbol]
+
+        if len(numbers) > 1:
+            raise ValueError("Hamiltonian must be expanded before using this method.")
+        elif numbers:
+            constant = float(numbers[0])
+        else:
+            constant = 1
+
+        if not symbols:
+            overall_constant += constant
+
+        elif len(symbols) == 1: 
+            target = symbol_num[symbols[0]]
+            Q[(target, target)] = constant
+
+        elif len(symbols) == 2:
+            target1 = symbol_num[symbols[0]]
+            target2 = symbol_num[symbols[1]]
+            Q[(target1, target2)] = constant
+
+        else:
+            raise ValueError("Only one and two qubit terms are allowed.")
+    
+    return Q, overall_constant
---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import numpy as np
+from qibo import hamiltonians, models, callbacks
+import functions
+import argparse
+
+
+def main(nqubits, instance, T, chainstrength, numruns, greedy, inspect):
+    """
+
+    Args:
+        nqubits (int): number of qubits for the file that contains the
+            information of an Exact Cover instance.
+        instance (int): intance used for the desired number of qubits.
+        T (float): 
+        
+
+    Returns:
+        
+    """
+    control, solution, clauses = functions.read_file(nqubits, instance)
+    nqubits = int(control[0])
+    times = functions.times(nqubits, clauses)
+    sh, smap = functions.h_problem(nqubits, clauses)
+    Q, constant = functions.symbolic_to_dwave(sh, smap)
+    
+    model = dimod.BinaryQuadraticModel.from_qubo(Q, offset = 0.0)
+    if not chainstrength:
+        chainstrength = dwave.embedding.chain_strength.uniform_torque_compensation(model)
+        print(f'Automatic chain strength: {chainstrength}\n')
+    else:
+        print(f'Chosen chain strength: {chainstrength}\n')
+        
+    sampler = EmbeddingComposite(DWaveSampler())
+    if greedy:
+        solver_greedy = SteepestDescentSolver()
+        sampleset = sampler.sample(model, chain_strength=chainstrength, num_reads=numruns, annealing_time=T, answer_mode='raw')
+        response = solver_greedy.sample(model, initial_states=sampleset)
+    else:
+        response = sampler.sample(model, chain_strength=chainstrength, num_reads=numruns, annealing_time=T)
+    
+    best_sample = response.record.sample[0]
+    best_energy = response.record.energy[0]
+    print(f'Best result found: {best_sample}\n')
+    print(f'With energy: {best_energy-constant}\n')
+    good_samples = response.record.sample[:min(len(response.record.sample), nqubits)]
+    good_energies = response.record.energy[:min(len(response.record.energy), nqubits)]
+    print(f'The best {len(good_samples)} samples found in the evolution are:\n')
+    for i in range(len(good_samples)):
+        print(f'Sample: {best_sample}    with energy: {best_energy-constant}\n')
+
+    if inspect:
+        dwave.inspector.show(response)
+
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--nqubits", default=8, type=int)
+    parser.add_argument("--instance", default=1, type=int)
+    parser.add_argument("--T", default=20, type=float)
+    parser.add_argument("--chainstrength", default=None, type=float)
+    parser.add_argument("--numruns", default=100, type=int)
+    parser.add_argument("--greedy", action="store_true")
+    parser.add_argument("--inspect", action="store_true")
+    args = vars(parser.parse_args())
+    main(**args)
